bdf78b38d64ac3a7f153cbe4f4abe2b8e8994414
ahmedNewiry <ahmedabdalfttah@outlook.com>
2025-05-16 22:33:17 +0300

    Use SQL aggregation and window functions to analyze data

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -25,3 +25,15 @@ FROM Property p
 LEFT JOIN Booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name, p.location, p.pricepernight
 ORDER BY booking_rank, p.property_id;
+
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.pricepernight,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name, p.location, p.pricepernight
+ORDER BY booking_rank, p.property_id;
